8876ffa3507ae2806c268f2978db9d5f06d32d98
WAshbourne22 <108470473+WAshbourne22@users.noreply.github.com>
2022-08-18 19:34:04 +0100

    Updates
    Changed one of the Divs to a nav and added .js so that users know what page they are on.

---
+++ index.js
@@ -0,0 +1,6 @@
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll ('div a'). forEach(link =>{
+    if(link.href.includes('\${activePage}')); {
+       link.classList.add('active');
+    }
+})
