1708d81c488583ea197c46c025362d770623ea76
WAshbourne22 <108470473+WAshbourne22@users.noreply.github.com>
2022-08-22 18:56:12 +0100

    removal of additional JS

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Let'a user know which active page they are on
 const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll ('div a'). forEach(link =>{
     if(link.href.includes('\${activePage}')); {
